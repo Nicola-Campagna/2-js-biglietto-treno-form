db23dd31b148d9322f37fe9d4abe93ce22ba7c66
Nicola-Campagna <campagnanicola379@gmail.com>
2023-04-03 17:51:42 +0200

    add style whit bootstrap

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-
-
+// AL CLICK SUL BOTTONE SUCCEDE QUESTO:
 btn.addEventListener(
     "click",
+    // funzione anonima
     function () {
-        // nome e cognome utente
+        // input nome e cognome utente
         const userNameSurname = document.getElementById("user");
-        // km da percorrere
+
+        // input km da percorrere
         const userKmEl = document.getElementById("km");
         // console.log(userKmEl.value);
 
-        // età passeggero
+        // input età passeggero
         const userAgeEl = document.getElementById("age");
         // console.log(userAgeEl.value);
 
+        // slot nel DOM per stampare il ticket finale
         const prezzoBigliettoEl = document.getElementById("ticket");
 
         // prezzo per km
@@ -21,41 +23,63 @@ btn.addEventListener(
         // ticket finale
         let ticket; //qui vale 0
 
-        // const ageValue = document.getElementById("eta");
-        // ageValue.innerHTML = userAgeEl.value;
-
 
         // GENERA PREZZO BIGLIETTO
 
         // SE MINORE DI 18 ANNI SCONTO DEL 20%
-        if (userAgeEl.value < 18) {
+        if (userAgeEl.value < 18 &&
+            !isNaN(userAgeEl.value) &&
+            userAgeEl.value > 0 &&
+            !isNaN(userKmEl.value) &&
+            userKmEl.value > 0
+        ) {
             ticket = ((price * userKmEl.value) * 20 / 100).toFixed(2);
             // console.log("under18: " + ticket);
-            prezzoBigliettoEl.innerHTML = "prezzo bilgietto under18: $" + ticket;
-
+            prezzoBigliettoEl.innerHTML += "under18: $" + ticket;
         }
         // ALTRIMENTI SE OVER 65 SCONTO DEL 40%
-        else if (userAgeEl.value > 65) {
+        else if (userAgeEl.value > 65 &&
+            !isNaN(userAgeEl.value) &&
+            userAgeEl.value > 0 &&
+            !isNaN(userKmEl.value) &&
+            userKmEl.value > 0
+        ) {
             ticket = ((price * userKmEl.value) * 40 / 100).toFixed(2);
             // console.log("over65: " + ticket);
-            prezzoBigliettoEl.innerHTML = "prezzo bilgietto over65: $" + ticket;
+            prezzoBigliettoEl.innerHTML += "over65: $" + ticket;
         }
         // ALTRIMENTI PREZZO PIENO
-        else {
+        else if (
+            !isNaN(userAgeEl.value) &&
+            userAgeEl.value > 0 &&
+            !isNaN(userKmEl.value) &&
+            userKmEl.value > 0
+        ) {
             ticket = (price * userKmEl.value).toFixed(2);
             // console.log("prezzo pieno: " + ticket);
-            prezzoBigliettoEl.innerHTML = "prezzo bilgietto pieno: $" + ticket;
+            prezzoBigliettoEl.innerHTML += "pieno: $" + ticket;
         }
+        else {
+            alert("VALORI INSERITI NON VALIDI");
+        }
+
+
 
         // STAMPIAMO I DATI INSERITI DALL'UTENTE NEL DOM ( COME UN BIGLIETTO )
 
         // km 
         const userKmValueEl = document.getElementById("kmValue");
-        userKmValueEl.innerHTML += userKmEl.value;
+        if (!isNaN(userKmEl.value) &&
+            userKmEl.value > 0) {
+            userKmValueEl.innerHTML += userKmEl.value;
+        }
 
         // età
         const userAgeValueEl = document.getElementById("ageValue");
-        userAgeValueEl.innerHTML += userAgeEl.value;
+        if (!isNaN(userAgeEl.value) &&
+            userAgeEl.value > 0) {
+            userAgeValueEl.innerHTML += userAgeEl.value;
+        }
 
         // nome e cognome
         const userNameSurnameValueEl = document.getElementById("userValue");
