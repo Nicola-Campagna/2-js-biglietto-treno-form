c8eb49a6ab1383dfa67beb3015c55693db8e4e55
Nicola-Campagna <campagnanicola379@gmail.com>
2023-04-19 11:51:04 +0200

    refactoring logic  js +  style

--- main.js
+++ main.js
@@ -1,88 +1,82 @@
+// input nome e cognome utente
+const userNameSurname = document.getElementById("user");
+
+// input km da percorrere
+const userKmEl = document.getElementById("km");
+// console.log(userKmEl.value);
+
+// input età passeggero
+const userAgeEl = document.getElementById("age");
+// console.log(userAgeEl.value);
+
+// slot nel DOM per stampare il ticket finale
+const prezzoBigliettoEl = document.getElementById("ticket");
+
 // AL CLICK SUL BOTTONE SUCCEDE QUESTO:
 btn.addEventListener(
     "click",
     // funzione anonima
     function () {
-        // input nome e cognome utente
-        const userNameSurname = document.getElementById("user");
-
-        // input km da percorrere
-        const userKmEl = document.getElementById("km");
-        // console.log(userKmEl.value);
-
-        // input età passeggero
-        const userAgeEl = document.getElementById("age");
-        // console.log(userAgeEl.value);
-
-        // slot nel DOM per stampare il ticket finale
-        const prezzoBigliettoEl = document.getElementById("ticket");
-
         // prezzo per km
         const price = 0.21;
-
         // ticket finale
         let ticket; //qui vale 0
 
+        // valore età passeggero
+        const age = userAgeEl.value;
+        // console.log(age);
 
-        // GENERA PREZZO BIGLIETTO
+        // volore km passeggero
+        const km = userKmEl.value;
+        // console.log(km);
 
-        // SE MINORE DI 18 ANNI SCONTO DEL 20%
-        if (userAgeEl.value < 18 &&
-            !isNaN(userAgeEl.value) &&
-            userAgeEl.value > 0 &&
-            !isNaN(userKmEl.value) &&
-            userKmEl.value > 0
-        ) {
-            ticket = ((price * userKmEl.value) * 20 / 100).toFixed(2);
-            // console.log("under18: " + ticket);
-            prezzoBigliettoEl.innerHTML += "under18: $" + ticket;
+        // SCONTO MINORENNE
+        const discountUnder18 = 20 / 100;
+
+        // SCONTO OVER 65
+        const discountOver65 = 40 / 100;
+
+        // SE MINORENNE APPLICARE SCONTO DEL 20%
+        if (age < 18 && age > 0 && !isNaN(age)) {
+            ticket = ((price * km) * discountUnder18).toFixed(2);
+            // console.log(ticket + ' under18');
         }
-        // ALTRIMENTI SE OVER 65 SCONTO DEL 40%
-        else if (userAgeEl.value > 65 &&
-            !isNaN(userAgeEl.value) &&
-            userAgeEl.value > 0 &&
-            !isNaN(userKmEl.value) &&
-            userKmEl.value > 0
-        ) {
-            ticket = ((price * userKmEl.value) * 40 / 100).toFixed(2);
-            // console.log("over65: " + ticket);
-            prezzoBigliettoEl.innerHTML += "over65: $" + ticket;
+        // ALTRIMENTI SE OVER-65 APPLICARE SCONTO DEL 40%
+        else if (age > 65 && age > 0 && !isNaN(age)) {
+            ticket = ((price * km) * discountOver65).toFixed(2);
+            // console.log(ticket + ' over65');
         }
         // ALTRIMENTI PREZZO PIENO
-        else if (
-            !isNaN(userAgeEl.value) &&
-            userAgeEl.value > 0 &&
-            !isNaN(userKmEl.value) &&
-            userKmEl.value > 0
-        ) {
-            ticket = (price * userKmEl.value).toFixed(2);
-            // console.log("prezzo pieno: " + ticket);
-            prezzoBigliettoEl.innerHTML += "pieno: $" + ticket;
-        }
         else {
-            alert("VALORI INSERITI NON VALIDI");
+            ticket = (price * km).toFixed(2);
+            // console.log(ticket + ' prezzo pieno');
         }
-
-
 
         // STAMPIAMO I DATI INSERITI DALL'UTENTE NEL DOM ( COME UN BIGLIETTO )
 
         // km 
         const userKmValueEl = document.getElementById("kmValue");
 
-        if (!isNaN(userKmEl.value) && userKmEl.value > 0) {
-            userKmValueEl.innerHTML += userKmEl.value;
+        if (!isNaN(km) && km > 0) {
+            userKmValueEl.innerHTML = km;
         }
 
-        // età
+        // // età
         const userAgeValueEl = document.getElementById("ageValue");
 
-        if (!isNaN(userAgeEl.value) && userAgeEl.value > 0) {
-            userAgeValueEl.innerHTML += userAgeEl.value;
+        if (!isNaN(age) && age > 0) {
+            userAgeValueEl.innerHTML = age;
         }
 
-        // nome e cognome
+        // // nome e cognome
         const userNameSurnameValueEl = document.getElementById("userValue");
-        userNameSurnameValueEl.innerHTML += (userNameSurname.value).toUpperCase();
+
+        userNameSurnameValueEl.innerHTML = (userNameSurname.value).toUpperCase();
+
+        // ticket finale 
+
+        const ticketEl = document.getElementById('ticket');
+
+        ticketEl.innerHTML = ticket + ' $';
     }
 )
